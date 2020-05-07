586623b854419c9515435d828c625f6490d9c352
Sebastian Alonso Gomez Arias <seagomezar@gmail.com>
2020-05-07 16:59:16 -0500

    Fixing console errors

--- components/Tag.js
+++ components/Tag.js
@@ -16,43 +16,43 @@ class Tag extends React.Component {
     const { name } = this.props;
     const tagColor = TagsColorDictionary[name] || "dark";
     return (
-      <Link href={"/tags/" + name}>
-        <a className={"badge badge-" + tagColor}>
-          {name}
+      //<Link href={"/tags/" + name}>
+      <a className={"badge badge-" + tagColor}>
+        {name}
 
-          <style jsx>{`
-            .badge {
-              text-decoration: none;
-              margin-right: 1em;
-              color: white;
-              padding: 0.5em 0.5em;
-              margin: 0.5em 0;
-              display: inline-block;
-            }
-            .badge-primary {
-              background-color: blue;
-            }
-            .badge-secondary {
-              background-color: green;
-            }
-            .badge-danger {
-              background-color: red;
-            }
-            .badge-warning {
-              background-color: #E5BE01;
-            }
-            .badge-info {
-              background-color: gray;
-            }
-            .badge-dark {
-              background-color: black;
-            }
-            .badge-success {
-              background-color: green;
-            }
-          `}</style>
-        </a>
-      </Link>
+        <style jsx>{`
+          .badge {
+            text-decoration: none;
+            margin-right: 1em;
+            color: white;
+            padding: 0.5em 0.5em;
+            margin: 0.5em 0;
+            display: inline-block;
+          }
+          .badge-primary {
+            background-color: blue;
+          }
+          .badge-secondary {
+            background-color: green;
+          }
+          .badge-danger {
+            background-color: red;
+          }
+          .badge-warning {
+            background-color: #e5be01;
+          }
+          .badge-info {
+            background-color: gray;
+          }
+          .badge-dark {
+            background-color: black;
+          }
+          .badge-success {
+            background-color: green;
+          }
+        `}</style>
+      </a>
+      //</Link>
     );
   }
 }
