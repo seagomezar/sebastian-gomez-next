84256f8d75213d67e6e5120180bf0d8a85fce45a
Sebastian Alonso Gomez Arias <seagomezar@gmail.com>
2020-05-07 17:57:29 -0500

    Pushing analytics

--- utils/analytics.js
+++ utils/analytics.js
@@ -2,7 +2,7 @@
 import ReactGA from "react-ga";
 export const initGA = () => {
   console.log("GA init");
-  ReactGA.initialize("UA-xxxxxxxxx-1");
+  ReactGA.initialize("UA-63483506-1");
 };
 export const logPageView = () => {
   console.log(`Logging pageview for ${window.location.pathname}`);
